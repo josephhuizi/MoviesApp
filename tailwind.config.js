59ae00e539d5f55ed1d11fbd7c369dda325a9a3f
Joseph Huizi <joseph.huizi@gmail.com>
2024-10-01 23:16:49 -0400

    sign in and sign up screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -24,7 +24,7 @@ module.exports = {
         plight: ["Poppins-Light", "sans-serif"],
         pregular: ["Poppins-Regular", "sans-serif"],
         pmedium: ["Poppins-Medium", "sans-serif"],
-        psemibold: ["Poppins-Semibold", "sans-serif"],
+        psemibold: ["Poppins-SemiBold", "sans-serif"],
         pbold: ["Poppins-Bold", "sans-serif"],
         pextrabold: ["Poppins-ExtraBold", "sans-serif"],
         pblack: ["Poppins-Black", "sans-serif"],
