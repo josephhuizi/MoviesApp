592d82f5eb6cafb0ab3153b6413a2d2e807cbec8
Joseph Huizi <joseph.huizi@gmail.com>
2024-10-02 17:54:05 -0400

    flat list to show videos

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,14 +1,25 @@
-import {View, Text, FlatList, Image} from 'react-native';
+import {View, Text, FlatList, Image, RefreshControl} from 'react-native';
 import {SafeAreaView} from "react-native-safe-area-context";
 
 import { images } from "../../constants";
 import SearchInput from "../../components/SearchInput";
+import Trending from "../../components/Trending";
+import EmptyState from "../../components/EmptyState";
+import {useState} from "react";
 
 const Home = () => {
+    const [refreshing, setRefreshing] = useState(false);
+
+    const onRefresh = async () => {
+        setRefreshing(true);
+        // re call videos
+        setRefreshing(false);
+    }
     return (
-        <SafeAreaView className="bg-primary">
+        <SafeAreaView className="bg-primary h-full">
             <FlatList
                 data={[{id: 1}, {id: 2}, {id: 3},]}
+                //data={[]}
                 keyExtractor={(item) => item.$id}
                 renderItem={({item}) => (
                     <Text className="text-3xl text-white">
@@ -35,8 +46,22 @@ const Home = () => {
                             </View>
                         </View>
                         <SearchInput />
+
+                        <View className="w-full flex-1 pt-5 pb-8">
+                            <Text className="text-lg font-pregular text-gray-100 mb-3">
+                                Latest Videos
+                            </Text>
+
+                            <Trending posts={[]}/>
+                        </View>
                     </View>
                 )}
+                ListEmptyComponent={() => (
+                    <EmptyState
+                        title="No videos found"
+                        subtitle="Be the first one to upload a video"/>
+                )}
+                refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />}
             />
         </SafeAreaView>
     );
--- components/SearchInput.jsx
+++ components/SearchInput.jsx
@@ -6,28 +6,20 @@ import { icons } from '../constants';
 const SearchInput = ({title, value, placeholder, handleChangeText, otherStyles, ...props}) => {
     const [showPassword, setShowPassword] = useState(false);
     return (
-        <View className={`space-y-2 ${otherStyles}`}>
-            <Text className="text-base text-gray-100 font-plight">
-                {title}
-            </Text>
+        <View className="flex flex-row items-center space-x-4 w-full h-16 px-4 bg-black-100 rounded-2xl border-2 border-black-200
+        focus:border-secondary-200">
+            <TextInput className="text-base mt-0.5 text-white flex-1 font-pregular"
+                       value={value}
+                       placeholder="Search for a video"
+                       placeholderTextColor="#7B7B8B"
+                       onChangeText={handleChangeText}
+                       />
 
-            <View className="w-full h-16 px-4 bg-black-100 rounded-2xl border-2 border-black-200
-            focus:border-secondary-200 flex flex-row items-center">
-                <TextInput className="flex-1 text-white font-psemibold text-base"
-                           value={value}
-                           placeholder={placeholder}
-                           placeholderTextColor="#7B7B8B"
-                           onChangeText={handleChangeText}
-                           secureTextEntry={title === "Password" && !showPassword}
-                           {...props}/>
-
-                {title === "Password" && (
-                    <TouchableOpacity onPress={() => setShowPassword(!showPassword)}>
-                        <Image source={!showPassword ? icons.eye : icons.eyeHide }
-                               className="w-6 h-6" resizeMode="contain" />
-                    </TouchableOpacity>
-                )}
-            </View>
+            <TouchableOpacity>
+                <Image source={icons.search}
+                       className="w-5 h-5"
+                       resizeMode='contain' />
+            </TouchableOpacity>
         </View>
     );
 }
